e3205b59206abd7d51a5f0747e3e97e74d32be84
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-09 23:16:58 +0300

    create custom followNumbers

---
+++ lib/views/widgets/custom_follow_numbers.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:mylinks/constants/colors.dart';
+import 'package:mylinks/constants/fonts.dart';
+
+class CustomFollowNumbers extends StatelessWidget {
+  final int number;
+  final String followName;
+  const CustomFollowNumbers({
+    required this.number,
+    required this.followName,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 75,
+      decoration: BoxDecoration(
+          color: AppColors.secondaryColor,
+          borderRadius: BorderRadius.circular(10)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('$number',style: Fonts.b24.copyWith(
+              color: AppColors.whiteColor
+          ),
+          ),
+          Text(followName,style: Fonts.r16.copyWith(
+              color: AppColors.offWhiteColor
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+}
