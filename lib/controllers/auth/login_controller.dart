39b9cd2e0c6af6ee9bccee8948de0cff6f95ea82
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-12 01:29:40 +0300

    auth feature

--- lib/controllers/auth/login_controller.dart
+++ lib/controllers/auth/login_controller.dart
@@ -1,16 +1,61 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mylinks/core/network/end_points.dart';
+import 'package:mylinks/core/network/remote/dio_helper.dart';
+import 'package:mylinks/models/user_login_model.dart';
+import 'package:mylinks/views/screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController extends GetxController {
   final formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final prefs = SharedPreferences.getInstance();
+
+  UserLoginModel? loginModel;
+
+  void userLogin({required String email, required String password}) {
+    DioHelper.postData(url: EndPoints.LOGIN, data: {
+      'email': email,
+      'password': password,
+    }).then((response) async {
+      if (formKey.currentState!.validate()) {
+      if (response.data != null) {
+        loginModel = UserLoginModel.fromJson(response.data);
+
+        if (response.statusCode == 200) {
+          final isHasToken = loginModel!.token != null;
+          if (isHasToken == true) {
+            Get.snackbar(
+              "Login",
+              "Successfully Login",
+              backgroundColor: Colors.green,
+              colorText: Colors.white,
+            );
+            emailController.clear();
+            passwordController.clear();
+            Get.offAllNamed(HomeScreen.route);
+          } else {
+            Get.snackbar(
+              "Failed",
+              "Login Failed",
+              backgroundColor: Colors.red,
+              colorText: Colors.white,
+            );
+          }
+        }
+      }}
+    }).catchError((error) {
+      print(error.toString());
+    });
+  }
 
   @override
   void onClose() {
     emailController.dispose();
     passwordController.dispose();
-
     super.onClose();
   }
 }
--- lib/controllers/auth/register_controller.dart
+++ lib/controllers/auth/register_controller.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mylinks/core/network/end_points.dart';
+import 'package:mylinks/core/network/remote/dio_helper.dart';
+import 'package:mylinks/models/register_model.dart';
 import 'package:mylinks/views/screens/auth/login_screen.dart';
-
+import 'package:mylinks/views/screens/home_screen.dart';
+import 'dart:developer' as dv;
 class RegisterController extends GetxController {
   final fullNameTextController = TextEditingController();
   final emailTextController = TextEditingController();
@@ -20,11 +24,56 @@ class RegisterController extends GetxController {
     super.onClose();
   }
 
-  void register() {
-    if (formKey.currentState!.validate()) {
-      // implemt register logic here
-      Get.offAllNamed(LoginScreen.route);
-    }
+  RegisterModel? registerModel;
+
+  void userRegister({
+    required String email,
+    required String password,
+    required String password_confirmation,
+    required String name,
+    required String phone,
+  }) {
+    DioHelper.postData(url: EndPoints.REGISTER, data: {
+      'email': email,
+      'password': password,
+      'name': name,
+      'phone': phone,
+      'password_confirmation': password_confirmation,
+    }).then((response) {
+      if (formKey.currentState!.validate()) {
+        dv.log(response.data.toString());
+        if (response.data != null) {
+          registerModel = RegisterModel.fromJson(response.data);
+          if (response.statusCode == 200 || response.statusCode == 201) {
+            final isHasToken = registerModel!.token != null;
+            if (isHasToken == true) {
+              Get.snackbar(
+                "Registered",
+                "User Created Successfully",
+                backgroundColor: Colors.green,
+                colorText: Colors.white,
+              );
+              fullNameTextController.clear();
+              emailTextController.clear();
+              passwordTextController.clear();
+              passwordTextController.clear();
+              confirmPasswordTextController.clear();
+              mobileTextController.clear();
+              Get.offAllNamed(HomeScreen.route);
+            } else {
+              Get.snackbar(
+                "Failed",
+                "something wrong",
+                backgroundColor: Colors.red,
+                colorText: Colors.white,
+              );
+            }
+          }
+        }
+      }
+    }).catchError((error) {
+      print(error.toString());
+    });
   }
 
   void signIn() {
