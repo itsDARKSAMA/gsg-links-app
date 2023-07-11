890d4a071e7ddc6034242d22115de737ec9272e5
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-07-11 18:32:00 +0300

    create ApiConstants

---
+++ lib/constants/api.dart
@@ -0,0 +1,7 @@
+
+class ApiConstants{
+  static const String baseUrl = 'http://osamapro.online/api/';
+  static const String headerKey = 'Content-Type';
+  static const String headerValue = 'application/json';
+  static const String authorizationKey = 'Authorization';
+}
